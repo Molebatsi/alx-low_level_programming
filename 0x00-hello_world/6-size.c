5bd09b363ad4de8c82f258ccc20a8d4a34c4d8ad
Obakeng Molebatsi <8141307+Molebatsi@users.noreply.github.com>
2023-07-13 19:28:37 +0200

    Update 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,1 +1,14 @@
+#include <stdio.h>
 
+int main() {
+    printf("Size of a char: %zu bytes\n", sizeof(char));
+    printf("Size of a short: %zu bytes\n", sizeof(short));
+    printf("Size of an int: %zu bytes\n", sizeof(int));
+    printf("Size of a long: %zu bytes\n", sizeof(long));
+    printf("Size of a long long: %zu bytes\n", sizeof(long long));
+    printf("Size of a float: %zu bytes\n", sizeof(float));
+    printf("Size of a double: %zu bytes\n", sizeof(double));
+    printf("Size of a long double: %zu bytes\n", sizeof(long double));
+
+    return 0;
+}
