d640af7a4e20d380c95c6c001b355c84ace923fa
Obakeng Molebatsi <8141307+Molebatsi@users.noreply.github.com>
2023-07-13 19:39:18 +0200

    Updated 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,5 +7,6 @@ int main(void)
     printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
     printf("Size of a long long int: %zu byte(s)\n", sizeof(long long));
     printf("Size of a float: %zu byte(s)\n", sizeof(float));
+    
     return 0;
 }
